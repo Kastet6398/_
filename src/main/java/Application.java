d3871838cc6c2ccbc36e5957e04570841b5a17f2
BobikGavvv <andrey678a@gmail.com>
2024-01-22 18:37:46 +0200

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+    }
+
+}
